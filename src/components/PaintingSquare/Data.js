0c95bb2d3d9e9090a5d60a0ba5f1cf38dd0fac31
Renzo <renzo.avila@travelgenio.com>
2020-03-18 17:00:01 -0300

    change url commit

--- src/components/PaintingSquare/Data.js
+++ src/components/PaintingSquare/Data.js
@@ -4,28 +4,28 @@ const Data = [
       litleImage: "/static/images/mini_image1.png",
       title: 'Desde 109.88 EUR por noche.',
       description: 'Palma de Mallorca',
-      link: 'https://' + window.location.hostname + '/hotels/search/CITY/2829/2020-05-22/2020-05-24/2/es'
+      link: 'https://' + window.location.hostname + '/hotels/search/METROPOLITAN_AREA/2829/2020-05-22/2020-05-24/2/es'
     },
     {
      urlImage: "/static/images/image2.png",
      litleImage: "/static/images/mini_image2.png",
      title: 'Desde 76.43 EUR por noche.',
      description: 'Lisboa',
-     link: 'https://' + window.location.hostname + '/hotels/search/CITY/2080/2020-05-22/2020-05-27/2/es'
+     link: 'https://' + window.location.hostname + '/hotels/search/METROPOLITAN_AREA/2080/2020-05-22/2020-05-27/2/es'
     },
     {
     urlImage: "/static/images/image3.png",
     litleImage: "/static/images/mini_image3.png",
     title: "Desde 87.39 EUR por noche.",
     description: 'Barcelona',
-    link: 'https://' + window.location.hostname + '/hotels/search/CITY/179992/2020-03-23/2020-03-26/2/es' 
+    link: 'https://' + window.location.hostname + '/hotels/search/METROPOLITAN_AREA/179992/2020-03-23/2020-03-26/2/es' 
     },
     {
     urlImage: "/static/images/image4.png",
     litleImage: "/static/images/mini_image4.png",
     title: 'Desde 200 EUR por noche.',
     description: 'Amsterdam',
-    link: 'https://' + window.location.hostname + '/hotels/search/CITY/178229/2020-03-23/2020-03-26/2/es'
+    link: 'https://' + window.location.hostname + '/hotels/search/METROPOLITAN_AREA/178229/2020-03-23/2020-03-26/2/es'
     }
   ];
 
