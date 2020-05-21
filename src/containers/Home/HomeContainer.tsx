da53b72543c88370d38f26406785b4eb018ce6ed
Ezequiel Kruk <ezequielkruk@travelgenio.com>
2020-05-21 13:10:40 -0300

    Change default dates

--- src/containers/Home/HomeContainer.tsx
+++ src/containers/Home/HomeContainer.tsx
@@ -1,59 +1,61 @@
-import React, { FunctionComponent, useEffect } from 'react';
-import { connect } from 'react-redux';
-import { RootState } from '../../store';
-import Home, { HomeProps } from '../../components/Home/Home';
-import { thunkSearchBoxChange } from '../../actions/searchBox/searchBox.action';
-import { fetchSuggestionSearch } from '../../actions/suggestion/suggestion.action';
+import React, { FunctionComponent, useEffect } from "react";
+import { connect } from "react-redux";
+import { RootState } from "../../store";
+import Home, { HomeProps } from "../../components/Home/Home";
+import { thunkSearchBoxChange } from "../../actions/searchBox/searchBox.action";
+import { fetchSuggestionSearch } from "../../actions/suggestion/suggestion.action";
 import { Container } from "@material-ui/core";
-import moment from 'moment';
-import { loadI18n } from '../../actions/i18n/i18n.action';
+import moment from "moment";
+import { loadI18n } from "../../actions/i18n/i18n.action";
 
-const HomeContainer: FunctionComponent<HomeProps> = props => {
-   useEffect(() => {
+const HomeContainer: FunctionComponent<HomeProps> = (props) => {
+  useEffect(() => {
     props.loadI18n();
   }, []);
- 
- return <Container maxWidth="lg">
-        <Home
-          initSearchBox = {
-            {
-              location: {
-                  code: '',
-                  type: ''
-              },
-              occupancy: {
-                  rooms: [{
-                      adults: 2,
-                      childrenAges: []
-                  }]
-              },
-              stay: {
-                  from: moment(),
-                  to: moment()
-              }
-            }
-          }
-          onChange={props.onChange}
-          onChangeSuggestionHint={props.onChangeSuggestionHint}
-          loading={props.loading}
-          suggestions={props.suggestions}
-          promotions= {props.promotions}
-          loadI18n={props.loadI18n}
-          />
-      </Container>;
-}
 
-const mapStateToProps = (rootState: RootState) => {
-    return {
-      suggestions: rootState.searchSuggestion.suggestions
-    };
+  const checkIn = moment().add(1, "month");
+  const checkOut = moment().add(1, "month").add(1, "week");
+
+  return (
+    <Container maxWidth="lg">
+      <Home
+        initSearchBox={{
+          location: {
+            code: "",
+            type: "",
+          },
+          occupancy: {
+            rooms: [
+              {
+                adults: 2,
+                childrenAges: [],
+              },
+            ],
+          },
+          stay: {
+            from: checkIn,
+            to: checkOut,
+          },
+        }}
+        onChange={props.onChange}
+        onChangeSuggestionHint={props.onChangeSuggestionHint}
+        loading={props.loading}
+        suggestions={props.suggestions}
+        promotions={props.promotions}
+        loadI18n={props.loadI18n}
+      />
+    </Container>
+  );
 };
 
-export default connect(
-    mapStateToProps,
-    {
-        onChange: thunkSearchBoxChange,
-        onChangeSuggestionHint: fetchSuggestionSearch,
-        loadI18n: loadI18n
-    }
-)(HomeContainer);
+const mapStateToProps = (rootState: RootState) => {
+  return {
+    suggestions: rootState.searchSuggestion.suggestions,
+  };
+};
+
+export default connect(mapStateToProps, {
+  onChange: thunkSearchBoxChange,
+  onChangeSuggestionHint: fetchSuggestionSearch,
+  loadI18n: loadI18n,
+})(HomeContainer);
