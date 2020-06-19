84ccb4c0325a5a346f6feaf69ff8c0fc4b54ca7c
Renzo <renzo.avila@travelgenio.com>
2020-06-19 16:25:07 -0300

    fix review

--- src/components/PaintingSquare/Data.js
+++ src/components/PaintingSquare/Data.js
@@ -9,25 +9,25 @@ const Data = [
       urlImage: "/static/images/image1.png",
       litleImage: "/static/images/mini_image1.png",
       description: 'Lisboa',
-      link: 'https://' + hostname + '/hotels/search/METROPOLITAN_AREA/178278/' + dateStart + '/' + dateEnd + '/2/es'
+      link: 'https://' + hostname + '/hotels/search/METROPOLITAN_AREA/178278/' + dateStart + '/' + dateEnd + '/2'
     },
     {
      urlImage: "/static/images/image2.png",
      litleImage: "/static/images/mini_image2.png",
      description: 'Palma de Mallorca',
-     link: 'https://' + hostname + '/hotels/search/CITY/2829/' + dateStart + '/' + dateEnd + '/2/es'
+     link: 'https://' + hostname + '/hotels/search/CITY/2829/' + dateStart + '/' + dateEnd + '/2'
     },
     {
     urlImage: "/static/images/image3.png",
     litleImage: "/static/images/mini_image3.png",
     description: 'Barcelona',
-    link: 'https://' + hostname + '/hotels/search/METROPOLITAN_AREA/179992/' + dateStart + '/' + dateEnd + '/2/es' 
+    link: 'https://' + hostname + '/hotels/search/METROPOLITAN_AREA/179992/' + dateStart + '/' + dateEnd + '/2' 
     },
     {
     urlImage: "/static/images/image4.png",
     litleImage: "/static/images/mini_image4.png",
     description: 'Amsterdam',
-    link: 'https://' + hostname + '/hotels/search/METROPOLITAN_AREA/178229/' + dateStart + '/' + dateEnd + '/2/es'
+    link: 'https://' + hostname + '/hotels/search/METROPOLITAN_AREA/178229/' + dateStart + '/' + dateEnd + '/2'
     }
   ];
 
